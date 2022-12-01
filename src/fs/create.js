112e66a337c6ac18331e6f2940bba15d4c24b2d9
Kirill Morgunov <kirillmorgunov@mail.ru>
2022-12-01 21:24:10 +0200

    feat: implement create method

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
-const create = async () => {
-    // Write your code here 
+import { access, constants, appendFile } from "node:fs/promises";
+
+const create = async () => { 
+  let isFileExists = false;
+
+  try {
+    await access("./files/fresh.txt", constants.R_OK | constants.W_OK);
+    isFileExists = true;
+  } catch {
+    isFileExists = false;
+  }
+
+  if (!isFileExists) {
+    await appendFile("./files/fresh.txt", "I am fresh and young");
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
