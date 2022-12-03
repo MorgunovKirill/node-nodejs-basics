b327be2c23181926ed56feef9389f2c6a41d90f0
Kirill Morgunov <kirillmorgunov@mail.ru>
2022-12-03 18:03:11 +0200

    feat: implement env method

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
-const parseEnv = () => {
-    // Write your code here 
+const parseEnv = () => {    
+    const result = [];
+    Object.keys(process.env).forEach((key) => {
+        if(key.includes('RSS_')) result.push(`${key}=${process.env[key]}`)
+    })
+
+    console.log(result.join('; '))
 };
 
 parseEnv();
