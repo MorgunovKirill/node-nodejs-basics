5c9aba30425e7dbe667c7d2d76750e20ebc77dcf
Kirill Morgunov <kirillmorgunov@mail.ru>
2022-12-03 20:19:58 +0200

    feat: implement streams|write method

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import path from 'node:path';
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { stdin } from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);       
+
+    stdin.on('data', (data) => {
+        const stream = createWriteStream(path.resolve(__dirname, 'files/fileToWrite.txt'));
+        stream.write(data, (err) => {
+            if(err) {
+                console.log(err);
+            } 
+        })
+    })
 };
 
 await write();
