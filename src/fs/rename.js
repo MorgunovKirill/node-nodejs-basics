efa4c61595f98c388fae568e31807853aa0ba7ee
Kirill Morgunov <kirillmorgunov@mail.ru>
2022-12-03 17:29:47 +0200

    feat: implement rename method

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import { access, constants, rename as fsRename } from "node:fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  let isWrongFilenameFileExists = false;
+  let isProperFilenameFileExists = false;
+
+  try {
+    await access("./files/wrongFilename.txt", constants.R_OK | constants.W_OK);
+    isWrongFilenameFileExists = true;
+  } catch {
+    isWrongFilenameFileExists = false;
+  }
+
+  try {
+    await access("./files/properFilename.md", constants.R_OK | constants.W_OK);
+    isProperFilenameFileExists = true;
+  } catch {
+    isProperFilenameFileExists = false;
+  }
+
+  if(isWrongFilenameFileExists && !isProperFilenameFileExists) {
+    fsRename('./files/wrongFilename.txt', './files/properFilename.md')
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
