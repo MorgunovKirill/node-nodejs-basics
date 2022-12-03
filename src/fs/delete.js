c0aa13ec3f603acba406ade3e54772c2e5027ef5
Kirill Morgunov <kirillmorgunov@mail.ru>
2022-12-03 17:35:37 +0200

    feat: implement delete method

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { access, constants, unlink } from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  let isFileExists = false;
+
+  try {
+    await access("./files/fileToRemove.txt", constants.R_OK | constants.W_OK);
+    isFileExists = true;
+  } catch {
+    isFileExists = false;
+  }
+
+  if (isFileExists) {
+    await unlink("./files/fileToRemove.txt");
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
