75f2ea60adfb6d6746d4701f60e729aca991111b
Kirill Morgunov <kirillmorgunov@mail.ru>
2022-12-04 19:58:23 +0200

    feat: add transfrom method

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from "stream";
+import { stdin, stdout } from 'node:process';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const transform = async () => {
-    // Write your code here 
+  const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.split('').reverse.join(''));
+    },
+  });
+
+  stdin.on('data', (data) => {   
+    const readStream = createReadStream(data);  
+    stdout.write(readStream.pipe(reverse))
+  })
+
 };
 
 await transform();
