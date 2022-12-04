60e6c93b3b0b399d3b988341cd388bb219cf8d92
Кирилл Моргунов <kirillmorgunov@mail.ru>
2022-12-04 22:33:50 +0200

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+  createReadStream,
+  createWriteStream
+} from 'node:fs';
+
 const compress = async () => {
-    // Write your code here 
+const gzip = createGzip();
+const source = createReadStream('./files/fileToCompress.txt');
+const destination = createWriteStream('archive.gz');
+
+pipeline(source, gzip, destination, (err) => {
+  if (err) {
+    console.error('An error occurred:', err);
+    process.exitCode = 1;
+  }
+});
 };
 
 await compress();
